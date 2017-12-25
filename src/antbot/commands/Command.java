2c10fda08dce0d5a23f5e73201bb943ea6dae255
eminguyen <emilyhuongnguyen@gmail.com>
2017-12-24 23:32:40 -0800

    Rewrite Command abstract class

--- src/antbot/commands/Command.java
+++ src/antbot/commands/Command.java
@@ -1,27 +1,52 @@
 package antbot.commands;
 
-import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
-public abstract class Command extends ListenerAdapter
+public abstract class Command
 {
-    public abstract void onCommand(MessageReceivedEvent e, String[] args);
+    private String name;
+    private String help;
     
-    @Override
-    public void onMessageReceived(MessageReceivedEvent e)
+    /**Default constructor for a command
+     * @param name the name of the command
+     * @param help the description of the command
+     */
+    public Command(String name, String help)
     {
-    	onCommand(e, commandArgs(e.getMessage()));
+        this.name = name;
+        this.help = help;
     }
     
-    protected String[] commandArgs(Message message)
+    /**Gives the name of a command
+     * @return String name of the command
+     */
+    public String getName()
     {
-    	return commandArgs(message.getContent());
+        return this.name;
     }
     
-    protected String[] commandArgs(String message)
+    /**Gives the description of the command
+     * @return String command's help info
+     */
+    public String getHelp()
     {
-    	return message.split(" ");
+        return this.help;
     }
-   
+    
+    /**Verifies if the command can be run
+     * @param args the arguments for the command
+     * @param event the event of the command's call
+     * @return String returns true if command is allowed, else returns
+     * an error message
+     */
+    public String allow(String[] args, MessageReceivedEvent event) {
+		return "The command has not been implemented";
+	}
+    
+    /**The action the command performs after being allowed
+     * @param args the arguments for the command
+     * @param event the event of the command's call
+     */
+    public void action(String[] args, MessageReceivedEvent event) {
+	}
 }
