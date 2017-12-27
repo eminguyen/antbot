fc5696911ad5fee780b8c713994a6e776b016996
eminguyen <emilyhuongnguyen@gmail.com>
2017-12-26 21:14:26 -0800

    Clean up main bot code

--- src/antbot/Ant.java
+++ src/antbot/Ant.java
@@ -5,10 +5,7 @@ import javax.security.auth.login.LoginException;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
-import net.dv8tion.jda.core.entities.*;
-import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.exceptions.*;
-import net.dv8tion.jda.core.hooks.*;
 
 public class Ant {
     public static void main(String[] args)
@@ -16,11 +13,11 @@ public class Ant {
     {
     	try
     	{
+    		//Initialize the bot; logs in and implements settings
     		Settings settings = new Settings("src/antbot/settings.properties");
             JDA jda = new JDABuilder(AccountType.BOT)
                 .setToken(settings.getToken())
                 .buildBlocking();
-            jda.addEventListener(new Ant());
     	}
     	catch(LoginException e)
     	{
