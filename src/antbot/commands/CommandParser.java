84e3ea1329b142cb61adeb1f90b5edd277b4fbe7
eminguyen <emilyhuongnguyen@gmail.com>
2017-12-29 23:33:00 -0800

    Add remove command name to parser

--- src/antbot/commands/CommandParser.java
+++ src/antbot/commands/CommandParser.java
@@ -3,19 +3,35 @@ package antbot.commands;
 import net.dv8tion.jda.core.entities.Message;
 
 public class CommandParser 
-{
-    public CommandParser()
-    {
-        
-    }
+{   
     
     public String[] getArguments(Message message)
     {
-        return message.toString().split(" ");
+        return removeFirst(splitMessage(message));
     }
     
     public String[] getArguments(String string)
+    {
+        return removeFirst(splitMessage(string));
+    }
+    
+    public String[] splitMessage(Message message)
+    {
+        return splitMessage(message.toString());
+    }
+    
+    public String[] splitMessage(String string)
     {   
         return string.split(" ");
+    }
+    
+    public String[] removeFirst(String[] array)
+    {
+        String[] newArray = new String[array.length-1];
+        for(int i = 1; i < array.length; i++)
+        {
+            newArray[i-1] = array[i];
+        }
+        return newArray;
     }
 }
