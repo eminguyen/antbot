5a14892287a96c64e4a3c1489ef07267908e05d5
eminguyen <emilyhuongnguyen@gmail.com>
2018-01-11 22:17:01 -0800

    Resolve compile issue by eliminating unnecessary exceptions. Clean up code

--- src/antbot/Ant.java
+++ src/antbot/Ant.java
@@ -5,32 +5,23 @@ import javax.security.auth.login.LoginException;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
-import net.dv8tion.jda.core.exceptions.*;
 
 public class Ant
 {
     public static void main(String[] args)
-        throws LoginException, RateLimitedException, InterruptedException
+    throws InterruptedException
     {
-    	try
-    	{
-    		//Initialize the bot; logs in and implements settings
-    		Settings settings = new Settings("src/antbot/settings.properties");
+        try
+        {
+            //Initialize the bot; logs in and implements settings
+            Settings settings = new Settings("src/antbot/settings.properties");
             JDA jda = new JDABuilder(AccountType.BOT)
-                .setToken(settings.getToken())
-                .buildBlocking();
+            .setToken(settings.getToken())
+            .buildBlocking();
     	}
-    	catch(LoginException e)
-    	{
-    		e.printStackTrace();
-    	}
-    	catch(InterruptedException e)
-    	{
-    		e.printStackTrace();
-    	}
-    	catch(RateLimitedException e)
-    	{
-    		e.printStackTrace();
-    	}
+        catch(LoginException | IllegalArgumentException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
