1892156d19246a8ae043f38b19c5db2aa5c21b54
eminnguyen <emilyhuongnguyen@gmail.com>
2018-03-26 02:11:18 -0700

    Add userid method

--- commands/essentials.js
+++ commands/essentials.js
@@ -2,13 +2,14 @@ module.exports = {
   commands: [
     "ping",
     "hello",
+    "userid"
   ],
 
   ping: {
     usage: "!ping",
     description: "Returns pong!",
     method: (client, message, argument) => {
-    message.channel.send("pong!");
+      message.channel.send("pong!");
     }
   },
 
@@ -16,7 +17,15 @@ module.exports = {
     usage: "!hello",
     description: "Tells the user hello",
     method: (client, message, argument) => {
-    message.channel.send("Hello " + message.author + "!");
+      message.channel.send("Hello " + message.author + "!");
+    }
+  },
+
+  userid: {
+    usage: "!userid",
+    description: "Tells the user their id",
+    method: (client, message, argument) => {
+      message.reply("your user id is \n" + message.author.id);
     }
   }
 
