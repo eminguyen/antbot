9ce38e8d5de68a49f5ff8b608a7ee30937603de8
eminnguyen <emilyhuongnguyen@gmail.com>
2018-03-28 02:13:09 -0700

    Add channel name change command

--- commands/channel.js
+++ commands/channel.js
@@ -1,18 +1,33 @@
 module.exports = {
   commands: [
-    "topic"
+    "topic",
+    "channelname"
   ],
 
   topic: {
-    usage: "!topic",
+    usage: "!topic <topic>",
     description: "Set the channel's topic",
     method: (client, message, argument) => {
       try {
         message.channel.setTopic(argument);
-        message.channel.send("The channel's topic has been set to " + argument);
+        message.channel.send("The channel's topic has been set to " + argument + ".");
       }
       catch(error) {
-        message.channel.send("I can't change the channel's topic");
+        message.channel.send("I can't change the channel's topic.");
+      }
+    }
+  },
+
+  channelname: {
+    usage: "!channelname <name of channel>",
+    description: "Set the channel's name",
+    method: (client, message, argument) => {
+      try {
+        message.channel.setName(argument);
+        message.channel.send("The channel's name has been set to " + argument + ".");
+      }
+      catch(error) {
+        message.channel.send("I can't change the channel's name.");
       }
     }
   }
