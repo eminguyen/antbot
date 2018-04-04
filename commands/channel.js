71c6b827bbbaf431035b2ac9412fd27956359518
eminnguyen <emilyhuongnguyen@gmail.com>
2018-04-03 23:09:29 -0700

    Add create voice channel command

--- commands/channel.js
+++ commands/channel.js
@@ -1,10 +1,11 @@
 module.exports = {
   commands: [
     "topic",
-    "channelname"
+    "channelname",
+    "createvoice",
   ],
 
-  topic: {
+  "topic": {
     usage: "!topic <topic>",
     description: "Set the channel's topic",
     method: (client, message, argument) => {
@@ -18,7 +19,7 @@ module.exports = {
     }
   },
 
-  channelname: {
+  "channelname": {
     usage: "!channelname <name of channel>",
     description: "Set the channel's name",
     method: (client, message, argument) => {
@@ -28,6 +29,21 @@ module.exports = {
       }
       catch(error) {
         message.channel.send("I can't change the channel's name.");
+      }
+    }
+  },
+
+  "createvoice": {
+    usage: "!createvoice <name of channel>",
+    description: "Creates a voice channel with the desired name",
+    method: (client, message, argument) => {
+      try {
+        message.guild.createChannel(argument, "voice");
+        message.channel.send("I have made the channel " + argument);
+      }
+      catch(error) {
+        console.log(error);
+        message.channel.send("I can't make a voice channel.");
       }
     }
   }
