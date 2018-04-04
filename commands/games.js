993235ae6c1a13aa0a91cfa43fcb303da3ebd872
eminnguyen <emilyhuongnguyen@gmail.com>
2018-04-03 23:02:36 -0700

    Add 8ball command

--- commands/games.js
+++ commands/games.js
@@ -2,9 +2,10 @@ module.exports = {
   commands: [
     "coinflip",
     "diceroll",
+    "8ball",
   ],
 
-  coinflip: {
+  "coinflip": {
     usage: "!coinflip",
     description: "Returns heads or tails!",
     method: (client, message, argument) => {
@@ -18,7 +19,7 @@ module.exports = {
     }
   },
 
-  diceroll: {
+  "diceroll": {
     usage: "!diceroll <number of sides> (optional)",
     description: "Rolls a dice with a given number of sides",
     method: (client, message, argument) => {
@@ -28,6 +29,41 @@ module.exports = {
       var result = (Math.floor(Math.random() * Math.floor(argument)));
       message.channel.send("I rolled " + (result + 1) + "!");
     }
-  }
+  },
 
+  "8ball" : {
+    usage: "!8ball <question>",
+    description: "Gives the user a response to their question",
+    method: (client, message, argument) => {
+      if(!argument) {
+        message.reply("Please ask me a question.");
+      }
+      else {
+        eightball = [
+        "It is certain.",
+        "It is decidedly so.",
+        "Without a doubt.",
+        "Yes definitely.",
+        "You may rely on it.",
+        "As I see it, yes.",
+        "Most likely.",
+        "Outlook good.",
+        "Yes.",
+        "Signs point to yes.",
+        "Reply hazy try again.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don't count on it.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "Very doubtful."
+        ];
+        var index = (Math.floor(Math.random() * Math.floor(20)));
+        message.reply(eightball[index]);
+      }
+    }
+  }
 }
