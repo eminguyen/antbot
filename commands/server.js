dedd0538d5446e8f2039728c8169fb04a1351700
eminnguyen <emilyhuongnguyen@gmail.com>
2018-04-04 17:28:57 -0700

    Add server owner command

---
+++ commands/server.js
@@ -0,0 +1,15 @@
+module.exports = {
+  commands: [
+    "owner",
+  ],
+
+  "owner": {
+    usage: "!owner",
+    description: "Gives the owner of the server",
+    method: (client, message, argument) => {
+      message.channel.send("The server's owner is " +
+      message.channel.guild.owner.user.username + ".");
+    }
+  },
+
+}
