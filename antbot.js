c910ef027d3fa73840cf7446e7eb2e7e68b9cbda
eminguyen <emilyhuongnguyen@gmail.com>
2018-04-04 23:11:57 -0700

    Merge branches

--- antbot.js
+++ antbot.js
@@ -97,10 +97,3 @@ var commandCheck = (message) => {
 client.on('message', message => {
   commandCheck(message);
 });
-
-client.login(token);
-
-var port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", function() {
-  console.log("Listening on Port 3000");
-});
