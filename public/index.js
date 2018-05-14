583233a0da527a13f0d28b5260b378e6393b9474
eminguyen <emilyhuongnguyen@gmail.com>
2018-05-14 13:49:24 -0700

    Add logo and animation to website

---
+++ public/index.js
@@ -0,0 +1,9 @@
+$('.ui.antbotico.image')
+  .transition('set looping')
+  .transition('pulse', '2000ms')
+;
+
+$('.ui.inviteico.button')
+  .transition('set looping')
+  .transition('tada', '2000ms')
+;
