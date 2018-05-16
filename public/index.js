c69c9b96bc210dde8a406418fce48ecf036bd0c5
eminguyen <emilyhuongnguyen@gmail.com>
2018-05-15 23:57:36 -0700

    Update website with more animations

--- public/index.js
+++ public/index.js
@@ -1,9 +1,18 @@
 $('.ui.antbotico.image')
   .transition('set looping')
-  .transition('pulse', '2000ms')
-;
+  .transition('pulse', '2000ms');
 
-$('.ui.inviteico.button')
-  .transition('set looping')
-  .transition('tada', '2000ms')
-;
+$( document ).ready(function() {
+  $('.ui.statistic')
+    .transition({
+      animation : 'drop in',
+      duration  : 1000,
+      interval  : 300
+    });
+});
+
+
+$('.ui.statistic').mouseenter( function(){
+    $(this).transition('tada');
+  }
+);
