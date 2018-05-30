f05c91f85f5faa9ed09cfa6c079760f5fc7302f9
eminguyen <emilyhuongnguyen@gmail.com>
2018-05-29 23:42:54 -0700

    Add second try catch block to finally resolve bot crashing

--- commands/fortnite.js
+++ commands/fortnite.js
@@ -21,51 +21,55 @@ module.exports = {
           }
         };
 
-        var stats;
-
         request(options, (error, response, body) => {
           if (!error && response.statusCode == 200) {
-            stats = JSON.parse(body);
+            var stats = JSON.parse(body);
           };
-        });
+          try {
+            let statsText =
+            {embed:
+              {
+                color: 3447003,
+                title: `Stats for ${stats.displayName}`,
+                fields: [
+                  {
+                    name: ':trophy: Victory Royales',
+                    value: `${stats.br.stats.pc.all.wins}`
+                  },
+                  {
+                    name: ':chart_with_upwards_trend: Win Rate',
+                    value: `${stats.br.stats.pc.all.winRate}%`
+                  },
+                  {
+                    name: ':gun: Kills',
+                    value: `${stats.br.stats.pc.all.kills}`
+                  },
+                  {
+                    name: ':skull: Deaths',
+                    value: `${stats.br.stats.pc.all.deaths}`
+                  },
+                  {
+                    name: ':black_heart: K/D',
+                    value: `${stats.br.stats.pc.all.kpd}`
+                  },
+                  {
+                    name: ':clock1: Time played',
+                    value: `${stats.br.stats.pc.all.minutesPlayed} minutes`
+                  },
+                ],
+                timestamp: new Date(),
+                footer: {
+                  text: "Fortnite Battle Royale"
+                }
+              }
+            };
 
-        let statsText =
-        {embed:
-          {
-            color: 3447003,
-            title: `Stats for ${stats.displayName}`,
-            fields: [
-              {
-                name: ':trophy: Victory Royales',
-                value: `${stats.br.stats.pc.all.wins}`
-              },
-              {
-                name: ':chart_with_upwards_trend: Win Rate',
-                value: `${stats.br.stats.pc.all.winRate}%`
-              },
-              {
-                name: ':gun: Kills',
-                value: `${stats.br.stats.pc.all.kills}`
-              },
-              {
-                name: ':skull: Deaths',
-                value: `${stats.br.stats.pc.all.deaths}`
-              },
-              {
-                name: ':black_heart: K/D',
-                value: `${stats.br.stats.pc.all.kpd}`
-              },
-              {
-                name: ':clock1: Time played',
-                value: `${stats.br.stats.pc.all.minutesPlayed} minutes`
-              },
-            ],
-            timestamp: new Date(),
-            footer: {
-              text: "Fortnite Battle Royale"
-            }
+            message.channel.send(statsText);
           }
-        };
+          catch (error) {
+            message.reply("I am unable to retrieve Fortnite statistics");
+          }
+        });
       }
       catch (error) {
         console.log(error);
