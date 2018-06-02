19db80703ec4d7deb1f7499a64bfcc6489e0a482
eminguyen <emilyhuongnguyen@gmail.com>
2018-06-02 00:10:31 -0700

    Add yoda speak command

---
+++ commands/language.js
@@ -0,0 +1,41 @@
+module.exports = {
+  commands: [
+    "yoda"
+  ],
+
+  "yoda": {
+    usage: "yoda",
+    description: "Translates your message to yoda speak",
+    method: (client, message, argument) => {
+      try {
+
+        var config = require("../config.json");
+
+        var request = require('request');
+
+        var options = {
+          headers: {
+            'X-Mashape-Key': config.mashape
+          },
+          method: "POST",
+          url: `https://yodish.p.mashape.com/yoda?text=${argument}`,
+          datatype: 'json',
+        }
+
+        request(options, (error, response, body) => {
+          if (!error && response.statusCode == 200) {
+            let yoda = JSON.parse(body);
+            message.channel.send(yoda.contents.translated);
+          }
+          else {
+            console.log(response);
+          }
+        })
+      }
+
+      catch(error) {
+        console.log(error);
+      }
+    }
+  }
+}
