e420d2c8e5353257f525ebbf6fb39bf9b6243b61
eminguyen <emilyhuongnguyen@gmail.com>
2018-06-02 00:38:17 -0700

    Add error catching to hearthstone command + emojis

--- commands/hearthstone.js
+++ commands/hearthstone.js
@@ -21,47 +21,51 @@ module.exports = {
         }
 
         request(options, (error, response, body) => {
-          var info = JSON.parse(body)[0];
-          console.log(info);
 
-          let infoText =
-          {embed:
-            {
-              color: 3447003,
-              title: `Card: ${info.name}`,
-              image: {
-                url: info.img,
-              },
-            fields: [
+          try {
+            var info = JSON.parse(body)[0];
+
+            let infoText =
+            {embed:
               {
-                name: 'Rarity',
-                value: info.rarity
-              },
-              {
-                name: 'Health',
-                value: info.health
-              },
-              {
-                name: 'Description',
-                value: info.flavor
-              },
-              {
-                name: 'Artist',
-                value: info.artist
+                color: 3447003,
+                title: `Card: ${info.name}`,
+                image: {
+                  url: info.img,
+                },
+              fields: [
+                {
+                  name: ':ring: Rarity',
+                  value: info.rarity
+                },
+                {
+                  name: ':heart: Health',
+                  value: info.health
+                },
+                {
+                  name: '"book: Description',
+                  value: info.flavor
+                },
+                {
+                  name: ':paintbrush: Artist',
+                  value: info.artist
+                }
+              ],
+                timestamp: new Date(),
+                footer: {
+                  text: "Hearthstone"
+                }
               }
-            ],
-              timestamp: new Date(),
-              footer: {
-                text: "Hearthstone"
-              }
-            }
-          };
-          message.channel.send(infoText);
+            };
+            message.channel.send(infoText);
+          }
+          catch (error) {
+            message.reply("I can't seem to find the card");
+          }
       });
       }
       catch (error) {
-        console.log(error);
-        message.reply("I am unable to retrieve Fortnite statistics");
+        message.reply("I can't seem to find the card");
       }
     },
   }
